ae76ef452cd33e0b2cb3d6af7517b60b45f13fcc
Filip Kaczmarek <f.7.kaczmarek@wp.pl>
2021-04-26 00:54:19 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+from wtforms import Form, StringField, SubmitField
+import re
+import pprint
+import json
+
+app = Flask(__name__)
+
+
+class SearchForm(Form):
+    user = StringField('Github Username')
+    submit = SubmitField('Search')
+
+@app.route('/', methods=['GET', 'POST'])
+def search_for_repositories():
+    form = SearchForm(request.form)
+    if request.method == 'POST':
+        return get_repositories(form)
+    return render_template('index.html', form=form)
+
+
+@app.route('/search_result')
+def get_repositories(search):
+
+    username = search.user.data
+    url = 'https://github.com/' + username
+    html = requests.get(url).text
+    response = requests.get(url)
+    response_code = response.status_code
+    if response_code != 200:
+        print("Error ocurred")
+        return
+    html_content = response.content
+    all_repositories = []
+    dom = BeautifulSoup(html_content, 'html.parser')
+
+    repo_attributes = dom.select("a.no-wrap, span.mr-3")
+    stars_list = []
+    for star in repo_attributes:
+        if ("octicon octicon-star" in str(star)):
+            star = str(star).splitlines()
+            if star[2] == "</span>":
+                star[2] = '0'
+            star[2] = star[2].replace(',', '')
+            stars_list.append(star[2].strip())
+
+    repositories = dom.select("div.flex-auto h3")
+    j = 0
+    for repo in repositories:
+        href_link = repo.a.attrs["href"]
+        url_link = "https://github.com{}".format(href_link)
+        username_replace = "/"+username+"/"
+        name = href_link.replace(username_replace, '', 1)
+        repo_list = [name, stars_list[j], url_link]
+        all_repositories.append(repo_list)
+        j += 1
+
+    all_repositories.sort(key=lambda x: int(x[1]), reverse=True)
+
+    return render_template('result.html', get_repositories = all_repositories)
+
+
+if __name__ == '__main__':
+
+    app.run(debug = True)
+
+    #<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    #<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
